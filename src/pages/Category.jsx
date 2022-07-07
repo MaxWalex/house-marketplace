b26eed0e58cd57353c6249503aa0e44ffc631e44
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2022-07-07 17:59:55 +0300

    Fetch Listings From Firebase

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+import {collection, getDocs, query, where, orderBy, limit, startAfter} from 'firebase/firestore'
+import {db} from '../firebase.config'
+import {toast} from 'react-toastify'
+import Spinner from '../components/Spinner'
 
 function Category() {
+    const [listings, setListings] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const params = useParams()
+
+    useEffect(() => {
+        const fetchListings = async () => {
+            try {
+                // Get reference
+                const listingsRef = collection(db, 'listings')
+
+                // Create a query
+                const q = query(
+                    listingsRef, 
+                    // where('type', '==', params.categoryName), 
+                    orderBy('timestamp', 'desc'), 
+                    limit(10)
+                )
+
+                // execure query
+                const querySnap = await getDocs(q)
+                
+                let listings = []
+                
+                querySnap.forEach(doc => {
+                    return listings.push({
+                        id: doc.id,
+                        data: doc.data()
+                    })
+                })
+
+                setListings(listings)
+                setLoading(false)
+            } catch (error) {
+                toast.error('Coul not fetch listings')
+            }
+        }
+
+        fetchListings()
+    }, [])
+
   return (
-    <div>Category</div>
+    <div className='category'>
+        <header>
+            <p className="pageHeader">
+                {params.categoryName === 'rent' ? 'Places for rent' : 'Places for sale'}
+            </p>
+        </header>
+
+        {loading ? ( 
+            <Spinner /> 
+        ) : listings && listings.length > 0 ? (
+            <>
+                <main>
+                    <ul className="categoryListings">
+                        {listings.map(listing => {
+                            return <h3>{listing.data.name}</h3>
+                        })}
+                    </ul>
+                </main>
+            </> 
+            // <p>No Listings for {params.categoryName}</p>
+        ) : ( 
+            <p>No Listings for {params.categoryName}</p>
+        )}
+    </div>
   )
 }
 
