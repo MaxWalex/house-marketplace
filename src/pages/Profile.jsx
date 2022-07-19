89c7250efb3769b800bc888948224e9b7028b68e
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2022-07-19 11:32:07 +0300

    Edit Listing Icon

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -87,6 +87,8 @@ function Profile() {
     }
   }
 
+  const onEdit = async (listingId) => navigate(`/edit-listing/${listingId}`)
+
   return <div className='profile'>
     <header className='profileHeader'>
       <p className="pageHeader">My Profile</p>
@@ -132,7 +134,7 @@ function Profile() {
           <p className="listingText">Your Listings</p>
           <ul className="listingsList">
             {listings.map((listing) => (
-              <ListingItem key={listing.id} listing={listing.data} id={listing.id} onDelete={() => onDelete(listing.id)} />
+              <ListingItem key={listing.id} listing={listing.data} id={listing.id} onDelete={() => onDelete(listing.id)} onEdit={() => onEdit(listing.id)} />
             ))}
           </ul>
         </>
